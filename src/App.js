016790afd74f67c5d0fd7b7203510d588d9f9a2c
Sizwe <91790158+Goveka@users.noreply.github.com>
2022-08-10 18:51:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ function App() {
   const searchInput=useRef();
   
 
+const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '2d6e8fcaf6msha33c7309355fa46p1987afjsnaa1e2d3b8078',
+        'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+      }
+    };
+
+    fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20`, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setMovieList(movies =>{
+          return data.d;
+        })
+      })
+      .catch(err => console.error(err));
+
   function search(){
     
     const search=searchInput.current.value;
